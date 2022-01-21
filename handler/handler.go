fb8838a657902ed46c3c7ce4f0cc501935aa1769
Teten Nugraha <teten.nugraha18@gmail.com>
2022-01-21 09:39:19 +0700

    update handler

---
+++ handler/handler.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"github.com/teten-nugraha/go-chi-simple/db"
+	"net/http"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/render"
+)
+
+var dbInstance db.Database
+
+func NewHandler(db db.Database) http.Handler {
+	router := chi.NewRouter()
+	dbInstance = db
+
+	router.MethodNotAllowed(methodNotAllowedHandler)
+	router.NotFound(notFoundHandler)
+	router.Route("/items", items)
+	return router
+}
+
+func methodNotAllowedHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(405)
+	render.Render(w, r, ErrMethodNotAllowed)
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(400)
+	render.Render(w, r, ErrNotFound)
+}
