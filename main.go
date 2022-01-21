085f889d804f5079fa7b4a723b37ee4cba353616
Teten Nugraha <teten.nugraha18@gmail.com>
2022-01-21 09:39:28 +0700

    update main.go

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/teten-nugraha/go-chi-simple/db"
+	"github.com/teten-nugraha/go-chi-simple/handler"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	addr := ":8080"
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Error occurred: %s", err.Error())
+	}
+	// load .env file from given path
+	// we keep it empty it will load .env from current directory
+	err = godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	dbUser, dbPassword, dbName :=
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB")
+	database, err := db.Initialize(dbUser, dbPassword, dbName)
+	if err != nil {
+		log.Fatalf("Could not set up database: %v", err)
+	}
+	defer database.Conn.Close()
+
+	httpHandler := handler.NewHandler(database)
+	server := &http.Server{
+		Handler: httpHandler,
+	}
+	go func() {
+		server.Serve(listener)
+	}()
+	defer Stop(server)
+	log.Printf("Started server on %s", addr)
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	log.Println(fmt.Sprint(<-ch))
+	log.Println("Stopping API server.")
+}
+func Stop(server *http.Server) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("Could not shut down server correctly: %v\n", err)
+		os.Exit(1)
+	}
+}
